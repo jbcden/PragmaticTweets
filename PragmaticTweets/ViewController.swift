652bb77bac48b6d3899b33465ad2a461f5e42046
Jacob Chae <jacob.chae@mobiledefense.com>
2015-11-15 18:21:38 -0500

    Initial Commit

---
+++ PragmaticTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PragmaticTweets
+//
+//  Created by Jacob Chae on 11/15/15.
+//  Copyright © 2015 Jooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
