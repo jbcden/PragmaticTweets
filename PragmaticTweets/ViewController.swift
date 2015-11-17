920ebc117a083549b0ad465e6c1e627ab4ffa93c
Jacob Chae <jacob.chae@mobiledefense.com>
2015-11-16 19:27:26 -0500

    Replace WebView with TableView

--- PragmaticTweets/ViewController.swift
+++ PragmaticTweets/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Social
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     @IBOutlet weak var twitterWebView: UIWebView!
 
     override func viewDidLoad() {
@@ -23,25 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func handleTweetButtonTapped(sender: UIButton) {
-        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
-            let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
-            tweetVC.setInitialText("I just finished the first project in iOS 9 SDK Development. #pragsios9")
-            self.presentViewController(tweetVC, animated: true, completion: nil)
-        } else {
-            NSLog("Can't send a tweet")
-        }
-    }
-   
-    @IBAction func handleShowMyTweetsTapped(sender: UIButton) {
-        reloadTweets()
+    func reloadTweets() {
     }
     
-    func reloadTweets() {
-        if let url = NSURL(string: "https://twitter.com/jbcden") {
-            let urlRequest = NSURLRequest(URL: url)
-            twitterWebView.loadRequest(urlRequest)
-        }
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section + 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
     }
     
 }
